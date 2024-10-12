c821b545a7ec869786e8b49d48b670ee8a948aad
truehazker <egoriusvulgaris@gmail.com>
2024-10-12 23:18:59 +0400

    feat: Added connectkit connector

--- src/const/wagmi-config.const.ts
+++ src/const/wagmi-config.const.ts
@@ -1,10 +1,16 @@
 import { http, createConfig } from 'wagmi';
-import { mainnet, sepolia } from 'wagmi/chains';
+import { mainnet } from 'wagmi/chains';
+import { getDefaultConfig } from 'connectkit';
 
-export const config = createConfig({
-  chains: [mainnet, sepolia],
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http()
-  }
-});
+export const config = createConfig(
+  getDefaultConfig({
+    appName: 'MultisigWallet',
+    walletConnectProjectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
+    chains: [mainnet /*goerli*/],
+    ssr: true,
+    transports: {
+      [mainnet.id]: http()
+      // [sepolia.id]: http(),
+    }
+  })
+);
--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import {
   QueryClient,
   QueryClientProvider
 } from '@tanstack/react-query';
+import { ConnectKitProvider } from 'connectkit';
 
 const queryClient = new QueryClient();
 
@@ -17,7 +18,9 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
-          <App/>
+          <ConnectKitProvider>
+            <App/>
+          </ConnectKitProvider>
         </QueryClientProvider>
       </WagmiProvider>
     </ThemeProvider>
