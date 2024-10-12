edf96899dae9d485c8a3696440864bd9855534b5
truehazker <egoriusvulgaris@gmail.com>
2024-10-12 22:58:19 +0400

    feat: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-4xl text-center mt-20">
+      Welcome to React 18
+    </h1>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '@/App.tsx';
+import '@/assets/styles/index.css';
+import { ThemeProvider } from './components/theme-provider.component';
+import { WagmiProvider } from 'wagmi';
+import { config } from '@/const/wagmi-config.const.ts';
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <App/>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
